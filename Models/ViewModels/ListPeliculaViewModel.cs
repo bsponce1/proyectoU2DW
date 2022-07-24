eed2f1e03387687e2c4a3a20f66f9f90ee66ea27
Steeven Engracia <saengracia@espe.edu.ec>
2022-07-24 12:37:09 -0500

    Se crea las clases con los atributos necesario para mostrar las películas al usuario y para ingresar la película.

---
+++ Models/ViewModels/ListPeliculaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoDesarrWebU2.Models.ViewModels
+{
+    public class ListPeliculaViewModel
+    {
+        public int idpel { get; set; }
+        public string titulopel { get; set; }
+        public Nullable<decimal> preciopel { get; set; }
+        public string imgpel { get; set; }
+
+
+
+    }
+}
---
+++ Models/ViewModels/PeliculaViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ProyectoDesarrWebU2.Models.ViewModels
+{
+    public class PeliculaViewModel
+    {
+        public int idpel { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Título")]
+        public string titulopel { get; set; }
+
+        [Required]
+        [Display(Name = "Precio")]
+        
+        public Nullable<decimal> preciopel { get; set; }
+
+        [Display(Name = "Subir imagen")]
+        public string imgpel { get; set; }
+
+        [NotMapped]
+        public HttpPostedFileBase imgFile { get; set; }
+
+    }
+}
