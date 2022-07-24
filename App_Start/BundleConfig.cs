27baab36dfd2e1b70538c231129912467efea07a
Steeven Engracia <saengracia@espe.edu.ec>
2022-07-24 12:24:42 -0500

    Se actualizarón las referencias a la versión más estable de Bootstrap.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,23 +8,27 @@ namespace ProyectoDesarrWebU2
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            {
+                bundles.Add(new Bundle("~/bundles/jquery").Include(
+                            "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                bundles.Add(new Bundle("~/bundles/jqueryval").Include(
+                            "~/Scripts/jquery.validate*"));
 
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                // Use the development version of Modernizr to develop with and learn from. Then, when you're
+                // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+                bundles.Add(new Bundle("~/bundles/modernizr").Include(
+                            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                          "~/Scripts/bootstrap.min.js"
+                          ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                bundles.Add(new Bundle("~/Content/css").Include(
+                          "~/Content/bootstrap.css",
+                          "~/Content/bootstrap.min.css",
+                          "~/Content/site.css"));
+            }
         }
     }
 }
